a266b8ae0b255315d80ec54e9199c75647cf49b1
Moritz Hipper <moritz.hipper@outlook.de>
2024-10-03 18:46:33 +0200

    added flyin animation for images

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,32 +1,39 @@
 import { useState } from "react"
+import room from "../assets/images/firstOutline.png"
 import lamp from "../assets/images/lamp.png"
 import plant from "../assets/images/plant.png"
 import "./Scene.css"
 
 export const Scene = () => {
   const [loadedImagesCount, setLoadedImagesCount] = useState(0)
-  const imagesCount = 2
+  const imagesCount = 3
 
   const finishedLoading = () => {
     setLoadedImagesCount((count) => count + 1)
   }
 
   if (loadedImagesCount === imagesCount) {
-    console.log("finished loading")
+    console.log("finished loading images")
   }
 
   return (
     <div className="scene-wrapper">
+      <img
+        onLoad={finishedLoading}
+        className="room flyIn3d"
+        src={room}
+        alt=""
+      />
       <div className="center">
         <img
           onLoad={finishedLoading}
-          className="scene lamp"
+          className="lamp flyIn3d"
           src={lamp}
           alt=""
         />
         <img
           onLoad={finishedLoading}
-          className="scene plant"
+          className="plant flyIn3d"
           src={plant}
           alt=""
         />
