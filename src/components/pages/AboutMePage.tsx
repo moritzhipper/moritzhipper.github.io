7208639117511e41440eadcc4a23a1f917dd2f60
Moritz Hipper <moritz.hipper@outlook.de>
2024-10-25 18:07:10 +0200

    wp

--- src/components/pages/AboutMePage.tsx
+++ src/components/pages/AboutMePage.tsx
@@ -1,26 +1,44 @@
+import me from "../../assets/images/me.webp"
+import meHiking from "../../assets/images/me_hiking.webp"
+import "./AboutMePage.css"
 import { InfoPageWrapper } from "./InfoPageWrapper"
 
 export const AboutMePage = () => {
   return (
-    <InfoPageWrapper header="About Me">
-      <img src="" alt="" />
-      <p className="big">
-        I was born in Frankfurt, moved to Stuttgart to study Information
-        Technology and have been building my career in Fintech for the past ten
-        years. I'm good in coding, connecting with others and sharing my
-        knowledge with students and colleagues. I really enjoy bringing my
-        creativity into my work.
-      </p>
-      <p className="big mt-s">
-        When I'm not coding, I'm probably in the nature somewhere looking at
-        birds.
-      </p>
-      <a
-        className="big mt-l"
-        href="https://www.linkedin.com/in/moritz-hipper-226226323/"
-      >
-        Contact me via Linkedin
-      </a>
+    <InfoPageWrapper header="About Me" className="about-me-page">
+      <div className="content">
+        <div className="images">
+          <img
+            className="me"
+            src={me}
+            alt="this is me looking into the camera like a responsible, competent developer does"
+          />
+          <img
+            className="hiking"
+            src={meHiking}
+            alt="this is me looking at a lake while hiking"
+          />
+        </div>
+        <div className="text">
+          <p className="big">
+            I was born in Frankfurt, moved to Stuttgart to study Information
+            Technology and have been building my career in Fintech for the past
+            ten years. I'm good in coding, connecting with others and sharing my
+            knowledge with students and colleagues. I really enjoy bringing my
+            creativity into my work.
+          </p>
+          <p className="big mt-s">
+            When I'm not coding, I'm probably in the nature somewhere looking at
+            birds.
+          </p>
+          <a
+            className="big mt-l"
+            href="https://www.linkedin.com/in/moritz-hipper-226226323/"
+          >
+            Contact me via Linkedin
+          </a>
+        </div>
+      </div>
     </InfoPageWrapper>
   )
 }
